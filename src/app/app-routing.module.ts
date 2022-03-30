5ccfb36babcd0bfb880cc6d233aa171cec8ebad1
Renzo Castro Jurado <renzo.vista@gmail.com>
2022-03-30 16:33:30 -0500

    Edición de template base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'quienes-somos', component: QuienesSomosComponent },
+  // { path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule,
