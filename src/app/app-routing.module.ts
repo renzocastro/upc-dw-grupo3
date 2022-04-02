b3afdc5075edc2da514256156c13d3332e5f6a8e
Rómulo Galindo <romulogalindo@gmail.com>
2022-04-02 01:56:19 -0500

    Módulo de gestión de empleados - p1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
+import {EmpleadosComponent} from "./private/gestion/empleados/empleados.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'quienes-somos', component: QuienesSomosComponent },
-  { path: 'servicios', component: ServiciosComponent}
+  { path: 'servicios', component: ServiciosComponent},
+  { path: 'privado/empleados', component: EmpleadosComponent}
   // { path: '',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { QuienesSomosComponent } from './pages/quienes-somos/quienes-somos.component';
 import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { EmpleadosComponent } from './private/gestion/empleados/empleados.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     QuienesSomosComponent,
-    ServiciosComponent
+    ServiciosComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
